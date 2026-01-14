d04f1adf2ad3df6aaed238ce47f8003537297dad
quanghuan <nquanghuan0806@gmail.com>
2026-01-14 12:24:02 +0700

    fix

--- FE/services/axiosConfig.ts
+++ FE/services/axiosConfig.ts
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const BASE_URL =
-  import.meta.env.MODE === "development"
-    ? import.meta.env.VITE_API_URL
-    : "http://localhost:5000/api";
+  import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api";
 // Tạo instance axios với config mặc định
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
